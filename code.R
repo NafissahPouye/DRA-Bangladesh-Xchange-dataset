7865dd045a5b1e041f3fefa83cc144ce7db7fa28
Nafissah Pouye <Nafissah Pouye>
2019-12-05 23:00:36 +0000

    update script

---
+++ code.R
@@ -0,0 +1,31 @@
+library(readxl)      #for excel, csv sheets manipulation
+library(sdcMicro)    #sdcMicro package with functions for the SDC process 
+library(tidyverse)   #for data cleaning
+
+#Import data
+setwd("C:/Users/LENOVO T46OS/Desktop/R scripts/DRA-Bangladesh-dataset")
+data <- read_excel("data.xlsx")
+col  <- colnames(data)
+selectedKeyVars <- c(col[2], col[3], col[4])
+weights <- c(col[38])
+
+#Convert variables into factors
+#cols =  c(col[2], col[3], col[4])
+data[,selectedKeyVars] <- lapply(data[,selectedKeyVars], factor)
+
+# Convert the sub file into dataframe
+subVars <- c(selectedKeyVars, weights)
+fileRes<-data[,subVars]
+fileRes <- as.data.frame(fileRes)
+
+#Assess the disclosure risk
+objSDC <- createSdcObj(dat = fileRes, keyVars = selectedKeyVars, 
+                       weightVar = weights)
+
+#Disclosure risk assessment 
+print(objSDC, "risk")
+
+#Generating an internal (extensive) report
+report(objSDC, filename = "index", internal = TRUE) 
+
+
